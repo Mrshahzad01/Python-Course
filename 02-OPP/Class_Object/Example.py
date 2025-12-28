67515b541adb4127203b4efeb086eb63d4547761
Mr Shahzad <mrshahzad7602@gmail.com>
2025-12-28 13:32:33 +0500

    Added CLass_object folder & Readme file

---
+++ 02-OPP/Class_Object/Example.py
@@ -0,0 +1,10 @@
+class car:
+    modal = "Toyota"
+    color = "White"
+    year = 2026
+
+
+my_car = car()   # Creating an object of class car
+print("My car modal is:", my_car.modal)
+print("My car color is :", my_car.color)
+print("My car year is :", my_car.year)
---
+++ 02-OPP/Class_Object/lec_01.py
@@ -0,0 +1,17 @@
+  #      Object Oriented Programming (OOP) in Python
+
+# level 1: Class and Object
+
+# A class is a blueprint for creating objects. An object is an instance of a class.
+class student:    # Defining a class named 'student'
+    name = "Ali"   # Class attribute 'name'
+    age = 23
+
+
+# Creating an object of the class student
+obj1 = student()
+print("Name:", obj1.name) # Accessing the class attribute 'name' using the object
+print("Age:", obj1.age)
+
+
+
